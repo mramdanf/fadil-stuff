74f1fe8114b685f5af46f7a654b6244bab29a6c1
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2022-07-16 12:52:59 +0700

    fix delete

--- rentalzzy/delet-peminjaman.php
+++ rentalzzy/delet-peminjaman.php
@@ -1,21 +1,31 @@
 <?php 
-include "koneksi_ezzy.php";
-$x = $_GET ['kode'];
-    $barang = mysqli_query(
-        $koneksi , "select * from barang_zzy where id_barang='$c'");
-    $stok = mysqli_fetch_array($barang);
-    $stok['stok'];
-    $a = mysqli_query(
-        $koneksi , "select * from peminjaman_zzy where id_peminjaman='$x'");
-    $b = mysqli_fetch_array($a);
-    $c = mysqli_fetch_array($a); // ngambil id barang
-    $c['id_barang'];
-    $d = $b['jumlah'];
-    $hasil = $stok + $d;
-    $tambah = mysqli_query(
-        $koneksi , "update barang_zzy set stok = '$hasil' where id_barang = $c");
-    $hapus = mysqli_query($koneksi , "delete from peminjaman_zzy where id_peminjaman='$x'");
-    if ($tambah|| $hapus){
-    header("location:peminjaman_zzy.php");
+    include "koneksi_ezzy.php";
+    $kodePeminjaman = $_GET ['kode'];
+
+    $getPeminjaman = mysqli_query($koneksi , "select * from peminjaman_zzy where id_peminjaman='$kodePeminjaman'");
+    $peminjaman = mysqli_fetch_assoc($getPeminjaman);
+    $getPeminjamanRow = mysqli_num_rows($getPeminjaman);
+    if ($getPeminjamanRow <= 0) {
+        echo "Query get peminjaman gagal";
+        exit();
     }
+
+    $jumlahDipinjam = $peminjaman['jumlah'];
+    $idBarang = $peminjaman['id_barang'];
+    $updateStock = mysqli_query($koneksi , "update barang_zzy set stok = stok + '$jumlahDipinjam' where id_barang = '$idBarang'");
+    $updateStockResult = mysqli_affected_rows($koneksi);
+    
+    if ($updateStockResult <= 0) {
+        echo "Query pengembalian stok gagal";
+        exit();
+    }
+
+    $deletePeminjaman = mysqli_query($koneksi , "delete from peminjaman_zzy where id_peminjaman='$kodePeminjaman'");
+    $deletePeminjamanResult = mysqli_affected_rows($koneksi);
+    if ($deletePeminjamanResult <= 0) {
+        echo "Query delete peminjaman gagal";
+        exit();
+    }
+
+    header("location:peminjaman_zzy.php");
 ?>
